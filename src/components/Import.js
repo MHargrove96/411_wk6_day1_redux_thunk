18a03e9f072c5536772ef07b4370cbee176cb032
MHargrove96 <MichaelH.96@yahoo.com>
2022-05-25 21:34:29 -0500

    final commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import {
+  Button,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+
+import { MoreVert, Delete } from "@material-ui/icons";
 
 const Import = (props) => {
-    // fill out this component
+  const [anchorEl, setAnchorEl] = useState(false);
 
-    return (
-        <p>Import Component</p>
-    )
-}
 
-export default Import
+  const handleMenuOpen = (e) => {
+    console.log(e.currentTarget, "event");
+    setAnchorEl(e.currentTarget);
+  };
+  const handleMenuclose = () => {
+    setAnchorEl(false);
+  };
+
+  const deleteRow = (i) => {
+    props.deleteMake(i);
+    handleMenuclose();
+  };
+  
+  const numOfRows = props.makes.length;
+
+  return (
+    <Container>
+      <h2>COUNT: {numOfRows}</h2>
+      <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+        Import
+      </Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((make, idx) => {
+            return (
+              <TableRow key={idx}>
+                <TableCell>{make.MakeId}</TableCell>
+                <TableCell>{make.MakeName}</TableCell>
+                <TableCell>
+                  <MoreVert onClick={handleMenuOpen} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleMenuclose}
+      >
+        <MenuItem onClick={deleteRow}>
+          <Delete />
+        </MenuItem>
+      </Menu>
+    </Container>
+  );
+};
+
+export default Import;
